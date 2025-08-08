e8cd92fc4add64f0bd53f78102b2b61602317037
PC_of_CXO <snozaki2017@gmail.com>
2025-08-08 09:13:42 +0900

    commit

--- assertion/assert.go
+++ assertion/assert.go
@@ -1,11 +1,24 @@
 package assertion
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/kinakoman/go-logger/logger"
+)
 
 type Assert struct {
-	t *testing.T
+	t      *testing.T
+	logger Logger
+}
+
+type Logger interface {
+	Error(message string)
 }
 
 func NewAssert(t *testing.T) *Assert {
-	return &Assert{t: t}
+	logger, err := logger.NewLogger()
+	if err != nil {
+		t.Fatalf("Failed to create logger: %v", err)
+	}
+	return &Assert{t: t, logger: logger}
 }
