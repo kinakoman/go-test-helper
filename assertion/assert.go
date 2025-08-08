1f819219f6826b1ae873dbe9f940bbb4fa2bd30a
PC_of_CXO <snozaki2017@gmail.com>
2025-08-08 09:23:58 +0900

    commit

--- assertion/assert.go
+++ assertion/assert.go
@@ -2,23 +2,12 @@ package assertion
 
 import (
 	"testing"
-
-	"github.com/kinakoman/go-logger/logger"
 )
 
 type Assert struct {
-	t      *testing.T
-	logger Logger
-}
-
-type Logger interface {
-	Error(message string)
+	t *testing.T
 }
 
 func NewAssert(t *testing.T) *Assert {
-	logger, err := logger.NewLogger()
-	if err != nil {
-		t.Fatalf("Failed to create logger: %v", err)
-	}
-	return &Assert{t: t, logger: logger}
+	return &Assert{t: t}
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module github.com/kinakoman/go-test-helper
 
 go 1.22.1
-
-require github.com/kinakoman/go-logger v0.0.0-20250808001057-723cb3593efd
-
-require (
-	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-)
